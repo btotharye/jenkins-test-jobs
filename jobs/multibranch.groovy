79318e1bd0177b7fb9843f3b459badcb7365d34e
Brian Hopkins <brianhh1230@gmail.com>
2022-03-04 16:48:03 +0000

    fixing multibranch code

--- jobs/multibranch.groovy
+++ jobs/multibranch.groovy
@@ -1,31 +1,15 @@
-multibranchPipelineJob('example') {
-  branchSources {
-    git {
-      // ...
-    }
-  }
-  factory {
-    remoteJenkinsFileWorkflowBranchProjectFactory {
-      remoteJenkinsFile(scriptPath)
-      localMarker('') /* everything is valid */
-      remoteJenkinsFileSCM {
-        gitSCM {
-          userRemoteConfigs	{
-            userRemoteConfig {
-              name('origin')
-              url('https://github.com/btotharye/python-jenkins-test.git')
-              refspec("+refs/heads/main:refs/remotes/origin/main")
-            }
-          }
-          branches {
-            branchSpec {
-              name('main')
-            }
-          }
-          browser {} // required, but doesn't require configuration
-          gitTool('git') // or wherever makes sense
+multibranchPipelineJob('python-example') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/btotharye/python-jenkins-test.git')
+            credentialsId('')
+            includes('JENKINS-*')
         }
-      }
     }
-  }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
 }
